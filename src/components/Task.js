3858e367f91b65ca85bcbb611abca1d3ca42f71e
Chris Maxwell <chmaxwel@icloud.com>
2021-10-08 16:43:23 +0200

    Update Task.js

--- src/components/Task.js
+++ src/components/Task.js
@@ -19,7 +19,6 @@ export default function Task({task: { id, title, state}, onArchiveTask, onPinTas
                 value={title} 
                 readOnly={true} 
                 placeholder="Input title"
-                style={{ background: 'red' }} 
             />
           </div>
     
